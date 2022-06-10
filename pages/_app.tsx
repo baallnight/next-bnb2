f58a33297e9605d6b37443feb7e96dbb2cdc56b0
syyoo <baallnight@naver.com>
2022-06-10 18:35:27 +0900

    second commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
+import Header from '../components/Header';
+import GlobalStyle from '../styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>    
+      <GlobalStyle/>
+      <Header/>
+      <Component {...pageProps} />
+      <div id="root-modal" />
+    </>
+  );
 }
 
 export default MyApp
